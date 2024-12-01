160ef5bc2d1381a5d2250a524535bf1cccb8f750
Matheus Reginato Rezende <contatorzn@hotmail.com>
2024-12-01 12:28:17 -0300

    initialize-project

---
+++ src/main/java/com/mvbackend/MvBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.mvbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvbackend/MvBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
