740bb4efa4febf54bca313f78a2bf107ce3d8a88
Matheus Reginato Rezende <contatorzn@hotmail.com>
2024-12-04 13:35:02 -0300

    refactored-data

--- src/main/java/com/mvbackend/domain/dto/DadosAtualizacaoAgendamento.java
+++ src/main/java/com/mvbackend/domain/dto/DadosAtualizacaoAgendamento.java
@@ -3,13 +3,14 @@ package com.mvbackend.domain.dto;
 import jakarta.validation.constraints.NotNull;
 
 import java.sql.Date;
+import java.time.LocalDate;
 
 public record DadosAtualizacaoAgendamento (
         @NotNull
         Long id,
 
         String descricao,
-        Date data,
+        LocalDate data,
         Long idVeiculo,
         Long idCliente,
         Long idServico
