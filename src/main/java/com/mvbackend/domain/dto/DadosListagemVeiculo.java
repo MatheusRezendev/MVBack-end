233882dbdf1bf32c6bb92dfcff277fbb03679ae9
Matheus Borges <matheusvbborges@hotmail.com>
2024-12-04 14:43:48 -0300

    alter-listagem-veiculo

--- src/main/java/com/mvbackend/domain/dto/DadosListagemVeiculo.java
+++ src/main/java/com/mvbackend/domain/dto/DadosListagemVeiculo.java
@@ -2,8 +2,8 @@ package com.mvbackend.domain.dto;
 
 import com.mvbackend.domain.model.Veiculo;
 
-public record DadosListagemVeiculo(Long id, String marca, String modelo, Integer ano) {
+public record DadosListagemVeiculo(Long id, String marca, String modelo, Integer ano, String nomeCliente) {
     public DadosListagemVeiculo( Veiculo veiculo ){
-        this(veiculo.getId(), veiculo.getMarca(), veiculo.getModelo(), veiculo.getAno());
+        this(veiculo.getId(), veiculo.getMarca(), veiculo.getModelo(), veiculo.getAno(), veiculo.getCliente().getNome());
     }
 }
